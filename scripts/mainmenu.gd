880db3093a83b0398c32854bbcae0d9ecb251232
hypnomacka <sovoman7@gmail.com>
2026-02-06 00:06:13 +0100

    feet: controls with dummy sprite

--- scripts/mainmenu.gd
+++ scripts/mainmenu.gd
@@ -16,6 +16,9 @@ func _on_quit_pressed() -> void:
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
-
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_controls_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/controls.tscn")
