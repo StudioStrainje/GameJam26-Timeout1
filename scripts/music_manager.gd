074c8b243e30b26361caa2d1e0b31b6ea0008436
Jake <dominik8655@gmail.com>
2026-02-06 03:22:24 +0100

    feat: add fade in/out for music

--- scripts/music_manager.gd
+++ scripts/music_manager.gd
@@ -8,7 +8,14 @@ var menu_scenes = [
 	"credits.tscn"
 ]
 
+const FADE_DURATION = 1.0
+const TARGET_VOLUME = -10.0
+const MUTE_VOLUME = -80.0
+
+var fade_tween: Tween
+
 func _ready():
+	audio_player.volume_db = MUTE_VOLUME
 	get_tree().tree_changed.connect(_on_tree_changed)
 	_check_scene()
 
@@ -23,17 +30,33 @@ func _check_scene():
 	var scene_name = current_scene.scene_file_path.get_file()
 	
 	if scene_name in menu_scenes:
-		_play_music()
+		_fade_in()
 	else:
-		_stop_music()
+		_fade_out()
 
-func _play_music():
+func _fade_in():
+	if fade_tween:
+		fade_tween.kill()
+	
 	if not audio_player.playing:
 		audio_player.play()
+	
+	fade_tween = create_tween()
+	fade_tween.tween_property(audio_player, "volume_db", TARGET_VOLUME, FADE_DURATION)
 
-func _stop_music():
-	if audio_player.playing:
-		audio_player.stop()
+func _fade_out():
+	if not audio_player.playing:
+		return
+	
+	if fade_tween:
+		fade_tween.kill()
+	
+	fade_tween = create_tween()
+	fade_tween.tween_property(audio_player, "volume_db", MUTE_VOLUME, FADE_DURATION)
+	fade_tween.finished.connect(_on_fade_out_finished)
+
+func _on_fade_out_finished():
+	audio_player.stop()
 
 func set_volume(volume_db: float):
 	audio_player.volume_db = volume_db
